cfdaedeff9a1a160d424223e134be73c1e7195b2
abdelalimah <abdelalietu@gmail.com>
2020-08-09 16:41:42 +0100

    Add default route fallback

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import routes from "./router";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import "react-bulma-components/lib";
 import history from "utils/history";
 
@@ -14,6 +19,7 @@ function App() {
               <route.component></route.component>
             </Route>
           ))}
+          <Redirect to="/home" />
         </Switch>
       </Router>
     </div>
