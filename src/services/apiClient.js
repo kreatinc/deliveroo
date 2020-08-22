c53580a787e710ee628af54f36cea2e5c188ab84
Abdelali Mahboub <45123407+abdelalimah@users.noreply.github.com>
2020-08-22 21:45:37 +0100

    Update apiClient.js
    Fix cross origin issue

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -6,7 +6,7 @@ const BASE_URL = "https://deliver-me-api.herokuapp.com/api";
 const apiClient = axios.create({
   baseURL: BASE_URL,
   timeout: 5000,
-  headers: { Accept: "application/json", "Content-Type": "application/json" },
+  headers: { Accept: "application/json", "Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
 });
 apiClient.interceptors.response.use(
   (response) => response,
