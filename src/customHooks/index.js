ca4615cc7c3f7a5cf50dc804596e5e95ff414f93
abdelalimah <abdelalietu@gmail.com>
2020-09-09 16:33:56 +0100

    Refactor custom auth hook

--- src/customHooks/index.js
+++ src/customHooks/index.js
@@ -2,33 +2,19 @@ import { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import routes from "router";
 
-export const useAuthenticated = () => {
+export const useAuthenticated = (item = "user") => {
   const [authenticated, setAuthenticated] = useState(false);
   const history = useHistory();
   const location = useLocation();
   useEffect(() => {
-    if (localStorage.getItem("user")) {
+    if (localStorage.getItem(item)) {
       setAuthenticated(true);
       if (
-        ["/", "/login", "/register", "/welcome"].includes(location.pathname)
+        ["/", "/login", "/register", "/welcome"].includes(location.pathname) &&
+        item === "user"
       ) {
         history.push("/home");
       }
-    }
-  }, []);
-  return authenticated;
-};
-export const useAuthenticatedCompany = () => {
-  const [authenticated, setAuthenticated] = useState(false);
-  const history = useHistory();
-  const location = useLocation();
-  useEffect(() => {
-    if (localStorage.getItem("company")) {
-      console.log("yes");
-      setAuthenticated(true);
-      history.push("/company");
-    } else {
-      history.push("/company/login");
     }
   }, []);
   return authenticated;
